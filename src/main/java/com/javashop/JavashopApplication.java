e3789ccce352c252fc0699f2ce224cb714d22b14
Sergio Guzmán <116006003+SergioPower@users.noreply.github.com>
2026-08-24 18:31:38 +0000

    chore: initialize Spring Boot project with PostgreSQL

---
+++ src/main/java/com/javashop/JavashopApplication.java
@@ -0,0 +1,13 @@
+package com.javashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavashopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavashopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javashop/JavashopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavashopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
