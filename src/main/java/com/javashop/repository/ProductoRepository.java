84cb98dad39d0e9d346eab598bad606893750673
Sergio Guzmán <116006003+SergioPower@users.noreply.github.com>
2026-08-24 19:19:23 +0000

    feat: cambiar CrudRepository a JpaRepository en ProductoRepository

--- src/main/java/com/javashop/repository/ProductoRepository.java
+++ src/main/java/com/javashop/repository/ProductoRepository.java
@@ -1,10 +1,10 @@
 package com.javashop.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.javashop.entity.Producto;
 
-public interface ProductoRepository extends CrudRepository<Producto, Long> {
+public interface ProductoRepository extends JpaRepository<Producto, Long> {
     
 
 }
